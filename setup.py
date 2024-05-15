b4fc319075573ca72d2bdc14287677e363c07ea4
Hugo <hugobloem@gmail.com>
2024-05-15 10:30:04 +0100

    Bump version from 1.0.6 to 1.0.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ data_files = [module_dir / "voices.json"]
 
 setup(
     name="wyoming_microsoft_tts",
-    version="1.0.5",
+    version="1.0.7",
     description="Wyoming Server for Microsoft TTS",
     url="https://github.com/hugobloem/wyoming-microsoft-tts",
     author="Hugo Bloem",
