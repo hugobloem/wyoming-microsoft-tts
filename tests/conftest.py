a70d81f0a008e90111f9c78cdb92588b9741519d
Hugo Bloem <hugobloem@gmail.com>
2025-02-23 18:24:51 +0000

    test: update test to specify a download directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,7 @@ def microsoft_tts(configuration):
     args = SimpleNamespace(
         subscription_key=os.environ.get("SPEECH_KEY"),
         service_region=os.environ.get("SPEECH_REGION"),
+        download_dir="/tmp/",
         **configuration
     )
     return MicrosoftTTS(args)
